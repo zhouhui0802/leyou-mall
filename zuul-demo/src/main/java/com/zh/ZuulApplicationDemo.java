3b273aff59e696dfd0b3bf370481e01a3ff8391c
zhouhui0802 <1741484908@qq.com>
2026-01-07 14:07:33 +0800

    Zuul网关的配置 自带的ribbon和hytrix

---
+++ zuul-demo/src/main/java/com/zh/ZuulApplicationDemo.java
@@ -0,0 +1,22 @@
+package com.zh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableZuulProxy  // 开启Zuul的网关功能
+@EnableDiscoveryClient
+public class ZuulApplicationDemo
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ZuulApplicationDemo.class, args);
+        System.out.println( "Hello World!" );
+    }
+}
